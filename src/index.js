472063465438af9b02c22532797c6217b09097ef
Maazbinsohail <maazbinsohail15@gmail.com>
2024-07-03 18:39:11 +0500

    all setup is configured

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require("dotenv").config();
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
 
 
@@ -10,7 +11,7 @@ dotenv.config({ path: "./env" });
 connectDB()
 .then(() =>{
   app.listen(process.env.PORT || 8000 , () => {
-    console.log(`Server started on port ${process.env.PORT}`);
+    console.log(`Server started on port  ${process.env.PORT}`);
   })
 })
 .catch((err) => {
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,7 @@
 
 
 const asyncHandler = (requestHandler) => {
-  async (req, res, next) => {
+  return (req, res, next) => {
     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
   };
 };
